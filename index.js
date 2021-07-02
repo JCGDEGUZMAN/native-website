df538b56aa0cb7b260d368cf89690a914f85684f
JCGDEGUZMAN <julius.d@cloudwalkdigital.com>
2021-07-02 15:32:15 +0800

    select menu persist and close menu

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+let url = new URL(window.location);
+let selectedMenu = url.searchParams.get("menu");
 const openMenuIcon = document.querySelector('.open-menu-icon');
 const closeMenuIcon = document.querySelector('.close-menu-icon');
 const menuList = document.querySelector('.page-header ul');
+const menuItems = document.querySelectorAll('.menu-list > li > a');
 
 window.addEventListener('resize', function(){
     const screenSize = window.innerWidth;
@@ -12,6 +15,11 @@ window.addEventListener('resize', function(){
         openMenuIcon.style.display = "flex";
         closeMenuIcon.className = "close-menu-icon";
     }
+})
+
+menuItems.forEach(item => {
+    const isActive = item.innerText.toLowerCase() === selectedMenu.toLowerCase();
+    item.className = isActive ? "menu-list-selected" : "menu-list-default";
 })
 
 openMenuIcon.addEventListener('click', function(){
